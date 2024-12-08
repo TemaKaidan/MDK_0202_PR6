3f5f005f9cdccda59ddf8295e7954c8ba27e6ef3
Artem <artemmohov05@mail.ru>
2024-12-09 00:32:01 +0500

    Исправлена ошибка с изображением. Мохов

--- PR6_RegIN_Mohov/Pages/Regin.xaml.cs
+++ PR6_RegIN_Mohov/Pages/Regin.xaml.cs
@@ -18,89 +18,157 @@ namespace PR6_RegIN_Mohov.Pages
     /// </summary>
     public partial class Regin : Page
     {
-        OpenFileDialog FileDialogImage = new OpenFileDialog();
-        private bool BSetImages = false;
+        OpenFileDialog opf = new OpenFileDialog();
+        bool BCorrectLogin = false;
+        bool BCorrectPassword = false;
+        bool BCorrectConfirmPassword = false;
+        bool BSetImages = false;
 
         public Regin()
         {
             InitializeComponent();
-
             MainWindow.mainWindow.UserLogIn.HandlerCorrectLogin += CorrectLogin;
             MainWindow.mainWindow.UserLogIn.HandlerInCorrectLogin += InCorrectLogin;
-
-            FileDialogImage.Filter = "PNG (*.png)|*.png|JPG (*.jpg)|*.jpg";
-            FileDialogImage.RestoreDirectory = true;
-            FileDialogImage.Title = "Choose a photo for your avatar";
+            opf.Filter = "PNG (*.png)|*.png|JPG (*.jpg)|*.jpg";
+            opf.RestoreDirectory = true;
+            opf.Title = "Choose a photo for your avatar";
         }
 
         private void CorrectLogin()
         {
             SetNotification("Login already in use.", Brushes.Red);
+            BCorrectLogin = false;
         }
+
         private void InCorrectLogin() => SetNotification("", Brushes.Black);
 
-        public void SetLogin()
+        private void SetLogin(object sender, KeyEventArgs e)
         {
-            if (IsValidLogin(TbLogin.Text))
+            if (e.Key == Key.Enter) SetLogin();
+        }
+
+        private void SetLogin(object sender, RoutedEventArgs e) => SetLogin();
+
+        void SetLogin()
+        {
+            Regex regex = new Regex(@"([a-zA-Z0-9._-]{4,}@[a-zA-Z0-9._-]{2,}\.[a-zA-Z0-9._-]{2,})");
+            BCorrectLogin = regex.IsMatch(TbLogin.Text);
+            if (regex.IsMatch(TbLogin.Text))
             {
                 SetNotification("", Brushes.Black);
                 MainWindow.mainWindow.UserLogIn.GetUserLogin(TbLogin.Text);
             }
-            else
-            {
-                SetNotification("Invalid login", Brushes.Red);
-            }
+            else SetNotification("Invalid login", Brushes.Red);
             OnRegin();
         }
-        public void SetPassword()
+
+        private void SetPassword(object sender, KeyEventArgs e)
         {
-            if (IsValidPassword(TbPassword.Password))
+            if (e.Key == Key.Enter) SetPassword();
+        }
+
+        private void SetPassword(object sender, RoutedEventArgs e) => SetPassword();
+
+        void SetPassword()
+        {
+            Regex regex = new Regex(@"(?=.*[0-9])(?=.*[!@#$%^&?*\-_=])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&?*\-_=]{10,}");
+            BCorrectPassword = regex.IsMatch(TbPassword.Password);
+            if (regex.IsMatch(TbPassword.Password) == true)
             {
                 SetNotification("", Brushes.Black);
-                ConfirmPassword(true);
+                if (TbConfirmPassword.Password.Length > 0) ConfirmPassword(true);
                 OnRegin();
             }
-            else
-            {
-                SetNotification("Invalid password", Brushes.Red);
-            }
+            else SetNotification("Invalid password", Brushes.Red);
         }
 
-        public bool ConfirmPassword(bool Pass = false)
+        private void ConfirmPassword(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Enter) ConfirmPassword();
+        }
 
-            if (TbConfirmPassword.Password != TbPassword.Password)
-            {
-                SetNotification("Passwords do not match", Brushes.Red);
-                return false;
-            }
+        private void ConfirmPassword(object sender, RoutedEventArgs e) => ConfirmPassword();
+
+        void ConfirmPassword(bool pass = false)
+        {
+            BCorrectConfirmPassword = TbConfirmPassword.Password == TbPassword.Password;
+            if (TbConfirmPassword.Password != TbPassword.Password) SetNotification("Password don't match", Brushes.Red);
             else
             {
                 SetNotification("", Brushes.Black);
-                if (!Pass) SetPassword();
-                return true;
-            }
-        }
-        void OnRegin()
-        {
-            if (IsFormValid())
-            {
-                MainWindow.mainWindow.UserLogIn.Login = TbLogin.Text;
-                MainWindow.mainWindow.UserLogIn.Password = TbPassword.Password;
-                MainWindow.mainWindow.UserLogIn.Name = TbName.Text;
-                if (BSetImages) MainWindow.mainWindow.UserLogIn.Image = File.ReadAllBytes("IUser.jpg");
-                MainWindow.mainWindow.UserLogIn.DateUpdate = DateTime.Now;
-                MainWindow.mainWindow.UserLogIn.DateCreate = DateTime.Now;
-                MainWindow.mainWindow.OpenPage(new Confirmation(Confirmation.TypeConfirmation.Regin));
+                if (!pass) SetPassword();
             }
         }
 
-        private bool IsValidLogin(string login) =>
-            new Regex(@"([a-zA-Z0-9._-]{4,}@[a-zA-Z0-9._-]{2,}\.[a-zA-Z0-9._-]{2,})").IsMatch(login);
-        private bool IsValidPassword(string password) =>
-            new Regex(@"(?=.*[0-9])(?=.*[!@#$%^&?*\-_=])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&?*\-_=]{10,}").IsMatch(password);
-        private bool IsFormValid() =>
-            !string.IsNullOrEmpty(TbName.Text) && IsValidLogin(TbLogin.Text) && IsValidPassword(TbPassword.Password) && ConfirmPassword(true);
+        void OnRegin()
+        {
+            if (!BCorrectLogin || TbName.Text.Length == 0 || !BCorrectPassword || !BCorrectConfirmPassword) return;
+            MainWindow.mainWindow.UserLogIn.Login = TbLogin.Text;
+            MainWindow.mainWindow.UserLogIn.Password = TbPassword.Password;
+            MainWindow.mainWindow.UserLogIn.Name = TbName.Text;
+            if (BSetImages) MainWindow.mainWindow.UserLogIn.Image = File.ReadAllBytes(Directory.GetCurrentDirectory() + @"\IUser.jpg");
+            MainWindow.mainWindow.UserLogIn.DateUpdate = DateTime.Now;
+            MainWindow.mainWindow.UserLogIn.DateCreate = DateTime.Now;
+            MainWindow.mainWindow.OpenPage(new Confirmation(Confirmation.TypeConfirmation.Regin));
+        }
+
+        private void SetName(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !(Char.IsLetter(e.Text, 0));
+        }
+
+        private void SelectImage(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (opf.ShowDialog() == true)
+                {
+                    using (Imaging.Image image = Imaging.Image.Load(opf.FileName))
+                    {
+                        int NewWidth = 0; int NewHeight = 0;
+                        if (image.Width > image.Height)
+                        {
+                            NewWidth = (int)(image.Width / (256f / image.Height));
+                            NewHeight = 256;
+                        }
+                        else
+                        {
+                            NewWidth = 256;
+                            NewHeight = (int)(image.Height * (256f / image.Width));
+                        }
+                        image.Resize(NewWidth, NewHeight);
+                        image.Save("IUser.jpg");
+                    }
+                    using (Imaging.RasterImage rasterImage = (Imaging.RasterImage)Imaging.Image.Load("IUser.jpg"))
+                    {
+                        if (!rasterImage.IsCached) rasterImage.CacheData();
+                        int X = 0; int Width = 256; int Y = 0; int Height = 256;
+                        if (rasterImage.Width > rasterImage.Height) X = (int)((rasterImage.Width - 256f) / 2);
+                        else Y = (int)((rasterImage.Height - 256f) / 2);
+                        Imaging.Rectangle rectangle = new Imaging.Rectangle(X, Y, Width, Height);
+                        rasterImage.Crop(rectangle);
+                        rasterImage.Save("IUser.jpg");
+                    }
+                    DoubleAnimation StartAnimation = new DoubleAnimation();
+                    StartAnimation.From = 1;
+                    StartAnimation.To = 0;
+                    StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+                    StartAnimation.Completed += delegate
+                    {
+                        IUser.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + @"\IUser.jpg"));
+                        DoubleAnimation EndAnimation = new DoubleAnimation();
+                        EndAnimation.From = 0;
+                        EndAnimation.To = 1;
+                        EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                        IUser.BeginAnimation(Image.OpacityProperty, EndAnimation);
+                    };
+                    IUser.BeginAnimation(Image.OpacityProperty, StartAnimation);
+                    BSetImages = true;
+                }
+                else BSetImages = false;
+            }
+            catch { MessageBox.Show($"Нельзя изменить выбранную картинку!"); }
+        }
 
         public void SetNotification(string Message, SolidColorBrush _Color)
         {
@@ -108,85 +176,6 @@ namespace PR6_RegIN_Mohov.Pages
             LNameUser.Foreground = _Color;
         }
 
-        private void SetLogin(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-                SetLogin();
-        }
-
-        private void SetLogin(object sender, RoutedEventArgs e) => SetLogin();
-        private void SetPassword(object sender, RoutedEventArgs e) => SetPassword();
-        private void ConfirmPassword(object sender, RoutedEventArgs e) => ConfirmPassword();
         private void OpenLogin(object sender, MouseButtonEventArgs e) => MainWindow.mainWindow.OpenPage(new Login());
-
-        private void SetName(object sender, TextCompositionEventArgs e) => e.Handled = !(Char.IsLetter(e.Text, 0));
-
-        private void SetPassword(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-                SetPassword();
-        }
-
-        private void ConfirmPassword(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-                ConfirmPassword();
-        }
-
-        private void SelectImage(object sender, MouseButtonEventArgs e)
-        {
-            if (FileDialogImage.ShowDialog() == true)
-            {
-                ProcessImage(FileDialogImage.FileName);
-                BSetImages = true;
-            }
-            else
-            {
-                BSetImages = false;
-            }
-        }
-
-        private void ProcessImage(string filePath)
-        {
-            using (Imaging.Image image = Imaging.Image.Load(filePath))
-            {
-                int NewWidth = image.Width > image.Height ? (int)(image.Width / (256f / image.Height)) : 256;
-                int NewHeight = image.Width > image.Height ? 256 : (int)(image.Height * (256f / image.Width));
-                image.Resize(NewWidth, NewHeight);
-                image.Save("IUser.jpg");
-            }
-
-            DisplayImage();
-        }
-        private void DisplayImage()
-        {
-            using (Imaging.RasterImage rasterImage = (Imaging.RasterImage)Imaging.Image.Load("IUser.jpg"))
-            {
-                if (!rasterImage.IsCached)
-                {
-                    rasterImage.CacheData();
-                }
-
-                int X = rasterImage.Width > rasterImage.Height ? (int)((rasterImage.Width - 256f) / 2) : 0;
-                int Y = rasterImage.Height > rasterImage.Width ? (int)((rasterImage.Height - 256f) / 2) : 0;
-
-                Imaging.Rectangle rectangle = new Imaging.Rectangle(X, Y, 256, 256);
-                rasterImage.Crop(rectangle);
-                rasterImage.Save("IUser.jpg");
-            }
-
-            AnimateImage();
-        }
-        private void AnimateImage()
-        {
-            DoubleAnimation startAnimation = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(0.6));
-            startAnimation.Completed += delegate
-            {
-                IUser.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + @"\IUser.jpg"));
-                DoubleAnimation endAnimation = new DoubleAnimation(0, 1, TimeSpan.FromSeconds(1.2));
-                IUser.BeginAnimation(System.Windows.Controls.Image.OpacityProperty, endAnimation);
-            };
-            IUser.BeginAnimation(System.Windows.Controls.Image.OpacityProperty, startAnimation);
-        }
     }
 }
